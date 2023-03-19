7dc4384b606d72a29e1801f81d71d15b3e3dfdf7
Olijoel0411 <oliverloconlopez@gmail.com>
2023-03-19 11:59:55 -0600

    creacion de clases

---
+++ ATM/src/clasesATM/Retiro.java
@@ -0,0 +1,20 @@
+package clasesATM;
+
+/**
+ *
+ * @author oliver
+ */
+
+
+public class Retiro {// inicio clase
+    
+    // Atributos
+    private int noCuenta; //  cuenta de la que se retirara el dinero
+    private double monto; // monto a retirar
+    
+    
+    // Constructor
+    public Retiro(){ }
+    
+    
+}// fin clase
